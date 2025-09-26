813369a6b0674f4574f99ea70902f7e3c2790be9
Igor Macêdo <macedoigor2003@gmail.com>
2025-09-26 10:29:02 -0300

    Atualizando a mensagem da aplicacao

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Olá, CI/CD com GitHub Actions e ArgoCD! aaaa"}
+    return {"message": "Olá, CI/CD com GitHub Actions e ArgoCD! bbb"}
 
 
 @app.get("/items/{item_id}")
