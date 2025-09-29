bc214c03ed9c09d2e027ce9dbd89db4215f0de88
Igor Macêdo <macedoigor2003@gmail.com>
2025-09-29 08:33:21 -0300

    Alteracao no main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Olá, CI/CD com GitHub Actions e ArgoCD! AOBA"}
+    return {"message": "Olá, CI/CD com GitHub Actions e ArgoCD!"}
 
 
 @app.get("/health")
